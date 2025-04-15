9919c98d58c98707507708c3ab91b73c41c0cdca
Tengku Arya Saputra <33353823+AryaSec1337@users.noreply.github.com>
2025-04-15 11:26:28 +0700

    Update HTML Injection - Mitigation.php

--- HTML Injection - Mitigation.php
+++ HTML Injection - Mitigation.php
@@ -1,11 +1,11 @@
 <?php
 // url : example.com/input?name=Tengku Arya Saputra
 // input : <scipt>alert(1)</script>
-$nama = $_GET['name']; // output <scritp>alert(1)</script>
-$nama = htmlspecialchars($_GET['name']); //output  &lt;script&gt;alert(&#039;XSS!&#039;)&lt;/script&gt; -> encode tag html
-$nama =  strip_tags($_GET['name']); // alert('XSS!') -> menghilangkan tag HTML
-$nama = htmlentities($_GET['name']); // &lt;script&gt;alert(&#039;XSS!&#039;)&lt;/script&gt; -> dijalankan sebagai seperti text biasa
-$name = htmlentities(strip_tags(htmlspecialchars($_GET['name']))); // Secure Validation
+$nama1 = $_GET['name']; // output <scritp>alert(1)</script>
+$nama2 = htmlspecialchars($_GET['name']); //output  &lt;script&gt;alert(&#039;XSS!&#039;)&lt;/script&gt; -> encode tag html
+$nama3 =  strip_tags($_GET['name']); // alert('XSS!') -> menghilangkan tag HTML
+$nama4 = htmlentities($_GET['name']); // &lt;script&gt;alert(&#039;XSS!&#039;)&lt;/script&gt; -> dijalankan sebagai seperti text biasa
+$nama5 = htmlentities(strip_tags(htmlspecialchars($_GET['name']))); // Secure Validation
 ?>
 
 <html>
@@ -13,6 +13,10 @@ $name = htmlentities(strip_tags(htmlspecialchars($_GET['name']))); // Secure Val
     <title>TEsted</title>
   </head>
   <body>
-    <span>Hallo semuanya nama saya <?= $nama ?></span>
+    <span>Vulnerable : <?= $nama1 ?></span><br>
+    <span>Htmlspecialchars : <?= $nama2 ?></span><br>
+    <span>Strip_tags : <?= $nama3 ?></span><br>
+    <span>Htmlentities : <?= $nama4 ?></span><br>
+    <span>Htmlentities + strip_tags + htmlspecialchars : <?= $nama5 ?></span><br>
   </body>
 </html>
